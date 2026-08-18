5a54daf21aef8361469ac6d45ccf914ed960b7b6
Fábio Junio <fjosf1@aluno.ifal.edu.br>
2026-08-18 17:48:28 -0300

    consertando a lógica do botão

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,43 @@ import { useState } from "react"
 import ChoiceButtom from './components/botao'
 
 export default function App() {
-  const [senha, changeSenha] = useState("")
+  const [senha, setSenha] = useState("")
+
+  // Recebe o número e concatena com o estado anterior
+  const addSenha = (numero) => {
+    setSenha((prevSenha) => prevSenha + numero)
+  }
+
+  // Remove o último caractere
+  const removeSenha = () => {
+    setSenha((prevSenha) => prevSenha.slice(0, -1))
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-[#4a0d12]">
       <div className="w-[1000px] h-[640px] bg-[url('/png-imagem.png')] bg-contain bg-center bg-no-repeat flex flex-col items-center justify-center">
-        <div>{senha}</div>
         <h1 className="text-xl font-bold text-gray-700">A senha vai aqui</h1>
-        {/* another conteiner */}
-        <div className="">
-          <ChoiceButtom numero={"1"} onclick={(e) => changeSenha(e.target.senha + num)}/>
-          <ChoiceButtom numero={"1"} onclick={(e) => changeSenha(e.target.senha + num)}/>
-          <ChoiceButtom numero={"1"} onclick={(e) => changeSenha(e.target.senha + num)}/>
-          <ChoiceButtom numero={"1"} onclick={(e) => changeSenha(e.target.senha + num)}/>
-          <ChoiceButtom numero={"1"} onclick={(e) => changeSenha(e.target.senha + num)}/>
-          <ChoiceButtom numero={"1"} onclick={(e) => changeSenha(e.target.senha + num)}/>
-          <ChoiceButtom numero={"1"} onclick={(e) => changeSenha(e.target.senha + num)}/>
-          <ChoiceButtom numero={"1"} onclick={(e) => changeSenha(e.target.senha + num)}/>
-          <ChoiceButtom numero={"1"} onclick={(e) => changeSenha(e.target.senha + num)}/>
-          <ChoiceButtom numero={"1"} onclick={(e) => changeSenha(e.target.senha + num)}/>
-          <ChoiceButtom numero={}/>
+        <p>{senha}</p>        
+        <div>
+          <div>
+            <ChoiceButtom valor="1" onClick={() => addSenha("1")} />
+            <ChoiceButtom valor="2" onClick={() => addSenha("2")} />
+            <ChoiceButtom valor="3" onClick={() => addSenha("3")} />
+          </div>
+          <div>
+            <ChoiceButtom valor="4" onClick={() => addSenha("4")} />
+            <ChoiceButtom valor="5" onClick={() => addSenha("5")} />
+            <ChoiceButtom valor="6" onClick={() => addSenha("6")} />
+          </div>
+          <div>
+            <ChoiceButtom valor="7" onClick={() => addSenha("7")} />
+            <ChoiceButtom valor="8" onClick={() => addSenha("8")} />
+            <ChoiceButtom valor="9" onClick={() => addSenha("9")} />
+          </div>
+          <ChoiceButtom valor="0" onClick={() => addSenha("0")} />
+          <ChoiceButtom valor="delete" onClick={removeSenha} />
         </div>
       </div>
-      
     </div>
   )
 }
--- src/components/botao.jsx
+++ src/components/botao.jsx
@@ -1,13 +1,7 @@
-import React from "react";
-import { useState } from "react";
-export default function ChoiceButtom(props){
-    const [value, changeValue] = useState(props.numero)
-    return(
-        <>
-            {/* ainda definirei o nome da classe para estilizar*/}
-            <div className="">
-                
-            </div>
-        </>
-    )
+export default function ChoiceButtom({ valor, onClick }) {
+  return (
+    <div onClick={onClick}>
+      {valor}
+    </div>
+  );
 }
